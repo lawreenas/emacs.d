6c3128cb79fa8e47fef846d442aee8759a0a978e
Laurynas Remeika <laurynas.remeika@backbase.com>
2016-01-20 23:27:39 +0200

    Move haskell settings to separate el file

--- init.el
+++ init.el
@@ -141,35 +141,14 @@
  '(helm-M-x-fuzzy-match t)
  '(helm-locate-fuzzy-match t))
 
+
+ (add-to-list
+  'load-path
+  (expand-file-name "settings" user-emacs-directory))
+
 ;; Haskell
 ;; =======
-(require 'haskell-mode)
-(require 'haskell-cabal)
-
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-
-(custom-set-variables
- '(haskell-process-type 'cabal-repl)
- '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t))
-
-(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
-(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
+;;(require 'lang-haskell)
 
 ;; Clojure
 ;; =======
@@ -506,7 +485,6 @@ Misc: [_a_]propos [_i_]nspect [_s_]cratch [_r_]efresh [_t_]race"
 ;; js2-mode settings
 ;; ===================
 
-;;(require 'js2-mode)
 (add-to-list
  'auto-mode-alist
  '("\\.js\\'" . js2-mode))
---
+++ settings/lang-haskell.el
@@ -0,0 +1,28 @@
+(require 'haskell-mode)
+(require 'haskell-cabal)
+
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+
+(custom-set-variables
+ '(haskell-process-type 'cabal-repl)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t))
+
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+(provide 'lang-haskell)
