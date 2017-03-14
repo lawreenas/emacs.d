6e4233bd6df662091cb7235f02d37d6653021f8b
Laurynas Remeika <laurynas@inventi.lt>
2017-03-14 20:48:46 +0200

    Use Ivy instead of Helm

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yasnippet eslintd-fix web-mode exec-path-from-shell json-mode rainbow-delimiters use-package flycheck zenburn-theme whitespace-cleanup-mode smartparens rjsx-mode markdown-mode magit helm-swoop helm-projectile expand-region company-tern beacon avy))))
+    (helm-ag counsel-projectile counsel ivy yasnippet eslintd-fix web-mode exec-path-from-shell json-mode rainbow-delimiters use-package flycheck zenburn-theme whitespace-cleanup-mode smartparens rjsx-mode markdown-mode magit helm-swoop helm-projectile expand-region company-tern beacon avy)))
+ '(projectile-globally-ignored-directories
+   (quote
+    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "build" "node_modules" "bower_components" "target"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-ivy.el
@@ -0,0 +1,42 @@
+;; Some shortcuts:
+;; C-c p f counsel-projectile-find-file: find a project file,
+;; C-c p d counsel-projectile-find-dir: find a project directory,
+;; C-c p b counsel-projectile-switch-to-buffer: switch to a project buffer,
+;; C-c p s s counsel-projectile-ag: search project files with ag,
+;; C-c p p counsel-projectile-switch-project: switch to another project (see above).
+
+(use-package ivy :ensure t
+  :diminish (ivy-mode . "")
+  :bind
+  (:map ivy-mode-map
+        ("C-c h" . counsel-projectile-find-file) ;; Search project files, just for helm habbit
+        ("C-c k" . counsel-yank-pop) ;; Show kill-ring
+        ("C-'" . ivy-avy))
+  :config
+  (ivy-mode 1)
+  (counsel-projectile-on) ;;https://github.com/ericdanan/counsel-projectile
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 10)   ;; number of result lines to display
+  ;; (setq ivy-count-format "")  ;; does not count candidates
+  (setq ivy-initial-inputs-alist nil)   ;; no regexp by default
+  (setq ivy-re-builders-alist   ;; configure regexp engine.
+        '((t   . ivy--regex-ignore-order))))         ;; allow input not in order
+
+
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+
+(provide 'init-ivy)
