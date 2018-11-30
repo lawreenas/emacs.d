92a285656c6d1650b724e1e7ad0de4903c5fce9c
Laurynas Remeika <laurynas@inventi.lt>
2018-11-30 21:54:15 +0200

    Check in custom.el file

--- custom.el
+++ custom.el
@@ -3,12 +3,54 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#ffffff" "#005cc5" "#6a737d" "#d73a49" "#6a737d" "#6a737d" "#6f42c1" "#6a737d"])
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "b35a14c7d94c1f411890d45edfb9dc1bd61c5becd5c326790b51df6ebf60f402" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" "151bde695af0b0e69c3846500f58d9a0ca8cb2d447da68d7fbf4154dcf818ebc" "b4c13d25b1f9f66eb769e05889ee000f89d64b089f96851b6da643cee4fdab08" "9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "281d7d85e178e8f38f46fd90751b85962c744c65c2373d24005027595ef58a74" "4667c741ff28edfceccbf258324bb03fd75b9e4b374c04e9bbcf16bdbf9ecb6d" "a84dd3da61ac3e8b343e40c50dab7c1fe1eea16665b0e46e625db08324681f0f" "8d1bd7e494ba006712bcf13464a4414e67b26da50b80e794c7872f0545c5dd8e" "863703fc30b58aa9b6276b1ccdf1df90c1c37bda07f95ac385e228b105e62086" "64059020fa8cea6b1a524d752ee0b3eacda99a70fd93273d22639d9241cc98de" "ba67bb5c6caaadb803155fff2eeac528a2adde8ad0ac411dac9842e719c5d170" "f27c9a62f43b7ac230efe7688c32deea62e2f26027690222462397f2c9a90a07" "ce80196f309436c6e76770e03ec0efd29d38b70d736d47b42ec6c0246b66045f" "37bdc463e80502c13876a16bda868980d9def1385b490aabc884159d0d3290d4" "171c35510b606d252f5e2af37e114636fe5b21246e5091c9d98207ade48fce39" "523d15f671f6e2ffb1e9f2c279236f8ab324248820eddb58b1a656ff8898ed74" "92acd0abaadba89f7474ade081412787df040995ab663d650228e350d887ba51" "6ab1b1c78f78ea8d9375a18e62946890be8b08de78823cc984ef1a960129aec4" "b9df706013b89d72be31f187b9d0ed566714bf7836dd210503296e669fba2330" "2954687611f840a61dc786c775da3f4dd34d3bd1d77495792024d9233970f35e" "5769e0e7a8b77bc50a15e7536c1752e5f197536cfa7c9040596fff8001e6b49a" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "16aa212ad6bee4d013c8d25a17bbd3b28707838f10da4cdc90f218b90693c8d5" "5ac4b43db49fa3530f54cb5781ac16eaa25858447d3caa71e67374eb31b2a159" "6dae91da5100791b40df2eb15c1838f5eebc4149f1ae11cbfbe38b1a9bd9eb3b" default)))
+ '(fci-rule-color "#6a737d")
+ '(helm-M-x-fuzzy-match t)
+ '(helm-buffers-fuzzy-matching t)
+ '(helm-locate-fuzzy-match t)
+ '(helm-recentf-fuzzy-match t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#f0f0f0" "#4078f2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#f0f0f0" "#50a14f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#f0f0f0" "#9ca0a4"))
+ '(nrepl-message-colors
+   (quote
+    ("#005cc5" "#6a737d" "#d73a49" "#6a737d" "#005cc5" "#6f42c1" "#d73a49" "#6a737d")))
  '(package-selected-packages
    (quote
-    (helm-ag counsel-projectile counsel ivy yasnippet eslintd-fix web-mode exec-path-from-shell json-mode rainbow-delimiters use-package flycheck zenburn-theme whitespace-cleanup-mode smartparens rjsx-mode markdown-mode magit helm-swoop helm-projectile expand-region company-tern beacon avy)))
+    (helm-rg anzu evil-escape evil doom-themes plantuml-mode helm-ag counsel-projectile counsel ivy yasnippet eslintd-fix web-mode exec-path-from-shell json-mode rainbow-delimiters use-package flycheck zenburn-theme whitespace-cleanup-mode smartparens rjsx-mode markdown-mode magit helm-swoop helm-projectile expand-region company-tern beacon avy)))
+ '(pdf-view-midnight-colors (quote ("#6a737d" . "#f8eec7")))
  '(projectile-globally-ignored-directories
    (quote
-    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "build" "node_modules" "bower_components" "target"))))
+    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".tox" ".svn" "build" "node_modules" "bower_components" "target")))
+ '(vc-annotate-background "#b0cde7")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#6a737d")
+     (40 . "#005cc5")
+     (60 . "#6a737d")
+     (80 . "#6a737d")
+     (100 . "#6a737d")
+     (120 . "#d73a49")
+     (140 . "#6a737d")
+     (160 . "#6a737d")
+     (180 . "#6a737d")
+     (200 . "#6a737d")
+     (220 . "#22863a")
+     (240 . "#005cc5")
+     (260 . "#6f42c1")
+     (280 . "#6a737d")
+     (300 . "#005cc5")
+     (320 . "#6a737d")
+     (340 . "#d73a49")
+     (360 . "#6a737d"))))
+ '(vc-annotate-very-old-color "#6a737d"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
